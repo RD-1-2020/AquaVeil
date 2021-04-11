c66bd5532e5f3bba3eda9547f4ce84b090d52bf8
Dimka neveDimka <mestrx@mail.ru>
2021-04-11 14:19:41 +0700

    123

--- AquaVeil/ucCanvas.cs
+++ AquaVeil/ucCanvas.cs
@@ -7,8 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections;
-using System.Diagnostics;
 
 namespace AquaVeil
 {
@@ -16,13 +14,10 @@ namespace AquaVeil
     {
 
         public clMap Map;
-        public Frames frames = new Frames(_distX, _distY);
-        public List<clMap> clMapList = new List<clMap>();
 
+        private Int32 _distX = 10;
+        private Int32 _distY = 10;
 
-        static private Int32 _distX = 10;
-        static private Int32 _distY = 10;
-        
         public ucCanvas()
         {
             InitializeComponent();
@@ -31,7 +26,7 @@ namespace AquaVeil
 
         public void Drawing()
         {
-            Graphics g = this._main.CreateGraphics();
+            Graphics g = pictureBox1.CreateGraphics();
 
             SolidBrush bb = new SolidBrush(Map.ColorPenBackground);
             SolidBrush bf = new SolidBrush(Map.ColorPenForeground);
@@ -39,17 +34,32 @@ namespace AquaVeil
             Pen pb = new Pen(bb);
             Pen pf = new Pen(bf);
             for (int i = 0; i < Map.Width; i++)
-                for (int j = 0; j < Map.Height; j++)
+                for (int j = 0; j < Map.Width; j++)
                 {
                     if (Map.MapCanvas[i][j] == 0)
                         b = bf;
                     else
                         b = bb;
                  
-                    g.FillRectangle(b, _distX+i* Map.PixelWidth, _distY+j*(Map.PixelWidth), Map.PixelWidth-2, Map.PixelHeight-2);
+                    g.FillRectangle(b, _distX+i* Map.PixelWidth, _distY+j*(Map.PixelWidth), Map.PixelWidth-2, Map.PixelHight-2);
                 }
 
         }
+        /// <summary>
+        /// Перерисовка одного пикселя
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        void Redraw_pixel(int x,int y) {
+            SolidBrush b;
+            Graphics g = pictureBox1.CreateGraphics();
+            if (Map.MapCanvas[x][y] == 0)
+                b = new SolidBrush(Map.ColorPenForeground);
+            else
+                b = new SolidBrush(Map.ColorPenBackground);
+                g.FillRectangle(b, _distX + x * Map.PixelWidth, _distY + y * (Map.PixelWidth), Map.PixelWidth - 2, Map.PixelHight - 2);
+        }
+
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
@@ -62,36 +72,32 @@ namespace AquaVeil
             Int32 y = e.Y;
 
             Int32 xx = (x - _distX) / Map.PixelWidth;
-            Int32 yy = (y - _distY) / Map.PixelHeight;
+            Int32 yy = (y - _distY) / Map.PixelHight;
 
             Map.InvertPixel(xx, yy);
+            //Redraw_pixel(xx,yy); Предложение (так как при больших размерах лагает и не оптимально)
             Drawing();
 
             toolStripStatusLabel1.Text = "X=" + x.ToString() + " Y=" + y.ToString() + " xx = " + xx.ToString() + " yy = " + yy.ToString();
         }
 
-        private void lb_clear_Click(object sender, EventArgs e)
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            Map.CreateCanvas();
-            Drawing();
+            if (e.Button != MouseButtons.Left)
+                return;
+            Int32 x = e.X;
+            Int32 y = e.Y;
+
+            Int32 xx = (x - _distX) / Map.PixelWidth;
+            Int32 yy = (y - _distY) / Map.PixelHight;
+
+            Map.InvertPixel(xx, yy);
+            Redraw_pixel(xx, yy);
         }
 
-        private void lb_savefr_Click(object sender, EventArgs e)
+        private void toolStripLabel1_Click(object sender, EventArgs e)
         {
-            Graphics g = pb_cadr_list.CreateGraphics();
-            g.Clear(Color.Gray);
-            clMapList.Add(Map);
-            frames = new Frames(_distX, _distY);
-            Point img_point = new Point(1, 1);
-            for (int i = 0; i < clMapList.Count; i++)
-            {
-                frames[i] = clMapList[i];
-                g.DrawImage(frames[i].pic, img_point);
-                img_point.X += (int)(_distX + Map.Width * Map.PixelWidth * frames.image_scale);
-            }
             Map = new clMap();
-            Debug.WriteLine(clMapList.Count);
-            Map.CreateCanvas();
             Drawing();
         }
     }
