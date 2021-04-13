1efada934076d2f20d954f221af2566338536163
Dimka neveDimka <mestrx@mail.ru>
2021-04-14 00:55:09 +0700

    good

--- AquaVeil/fMain.cs
+++ AquaVeil/fMain.cs
@@ -16,33 +16,7 @@ namespace AquaVeil
         public fMain()
         {
             InitializeComponent();
-            propertyGrid1.SelectedObject = ucCanvas.Map;
-        }
-
-        private void ucCanvas1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void toolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            add_frame("drink",1);
-        }
-        public void add_frame(String name_frame, int num_frame)
-        {
-
-            Graphics g = ucCanvas.pictureBox1.CreateGraphics();
-            ucCanvas.pictureBox1.Invalidate();
-            Image image = ucCanvas.pictureBox1.Image;
-            image.Crop(new Rectangle(0, 0, ucCanvas.Map.Width * ucCanvas.Map.PixelWidth + 2 * ucCanvas.Map.Width,
-                    ucCanvas.Map.Hight * ucCanvas.Map.PixelHight + 2 * ucCanvas.Map.Hight));
-            ucCanvas.listView1.StateImageList.Images.Add(image);
-            ucCanvas.listView1.Items.Add(name_frame, num_frame);
+            //propertyGrid1.SelectedObject = clMap;
         }
     }
 }
