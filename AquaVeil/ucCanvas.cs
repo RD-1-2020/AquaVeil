ee35ebf1c6f2b101ce030e7ddc71eab575db5f6c
Dimka neveDimka <mestrx@mail.ru>
2021-04-14 00:55:09 +0700

    good

--- AquaVeil/ucCanvas.cs
+++ AquaVeil/ucCanvas.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
 
 namespace AquaVeil
 {
@@ -14,43 +16,40 @@ namespace AquaVeil
     {
 
         public clMap Map;
+        public Frames frames = new Frames(_distX, _distY);
+        public List<clMap> clMapList = new List<clMap>();
 
-        private Int32 _distX = 10;
-        private Int32 _distY = 10;
 
+        static private Int32 _distX = 10;
+        static private Int32 _distY = 10;
+        
         public ucCanvas()
         {
             InitializeComponent();
             Map = new clMap();
         }
-        
-        
+
         public void Drawing()
         {
-            var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.Image = bmp;
-            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Graphics g = this._main.CreateGraphics();
 
             SolidBrush bb = new SolidBrush(Map.ColorPenBackground);
             SolidBrush bf = new SolidBrush(Map.ColorPenForeground);
             SolidBrush b;
             Pen pb = new Pen(bb);
             Pen pf = new Pen(bf);
-
             for (int i = 0; i < Map.Width; i++)
-                for (int j = 0; j < Map.Width; j++)
+                for (int j = 0; j < Map.Height; j++)
                 {
                     if (Map.MapCanvas[i][j] == 0)
                         b = bf;
                     else
                         b = bb;
                  
-                    g.FillRectangle(b, _distX+i* Map.PixelWidth, _distY+j*(Map.PixelWidth)
-                        , Map.PixelWidth-2, Map.PixelHight-2);
+                    g.FillRectangle(b, _distX+i* Map.PixelWidth, _distY+j*(Map.PixelWidth), Map.PixelWidth-2, Map.PixelHeight-2);
                 }
-            
-        }
 
+        }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
@@ -63,24 +62,37 @@ namespace AquaVeil
             Int32 y = e.Y;
 
             Int32 xx = (x - _distX) / Map.PixelWidth;
-            Int32 yy = (y - _distY) / Map.PixelHight;
+            Int32 yy = (y - _distY) / Map.PixelHeight;
 
             Map.InvertPixel(xx, yy);
             Drawing();
 
             toolStripStatusLabel1.Text = "X=" + x.ToString() + " Y=" + y.ToString() + " xx = " + xx.ToString() + " yy = " + yy.ToString();
         }
-    }
-    static class ext {
-        public static Image Crop(this Image image, Rectangle selection)
+
+        private void lb_clear_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = image as Bitmap;
-            if (bmp == null)
-                throw new ArgumentException("No valid bitmap");
-            // Crop the image:
-            Bitmap cropBmp = bmp.Clone(selection, bmp.PixelFormat);
-            image.Dispose();
-            return cropBmp;
+            Map.CreateCanvas();
+            Drawing();
+        }
+
+        private void lb_savefr_Click(object sender, EventArgs e)
+        {
+            Graphics g = pb_cadr_list.CreateGraphics();
+            g.Clear(Color.Gray);
+            clMapList.Add(Map);
+            frames = new Frames(_distX, _distY);
+            Point img_point = new Point(1, 1);
+            for (int i = 0; i < clMapList.Count; i++)
+            {
+                frames[i] = clMapList[i];
+                g.DrawImage(frames[i].pic, img_point);
+                img_point.X += (int)( Map.Width * (Map.PixelWidth * frames.image_scale)-2/frames.image_scale);
+            }
+            Map = new clMap();
+            Debug.WriteLine(clMapList.Count);
+            Map.CreateCanvas();
+            Drawing();
         }
     }
 }
