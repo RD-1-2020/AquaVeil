2ce504b608880d2ba52e359e16b0154a03a8b661
RD-1-2020 <mestrx@mail.ru>
2021-06-19 00:33:01 +0700

    wpf

---
+++ WaterfallSwingWPF/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AquaVeilV1;
+
+namespace WaterfallSwingWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Положение редактируемого кадра
+        /// </summary>
+        Int32 posX = 20;
+
+        /// <summary>
+        /// Положение редактируемого кадра
+        /// </summary>
+        Int32 posY = 40;
+
+        private clMap Map;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static BitmapSource ToBitmapSource(DrawingImage source)
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawImage(source, new Rect(new Point(0, 0), new Size(source.Width, source.Height)));
+            drawingContext.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)source.Width, (int)source.Height, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+            return bmp;
+        }
+
+        private void Drawing()
+        {
+            if (Map == null)
+                return;
+
+            DrawingImage frameBitmap = new DrawingImage();
+
+            GeometryDrawing g = new GeometryDrawing();
+
+            frameBitmap.Drawing = Map.getBitmapImage();
+
+            MainCanvas.Source = ToBitmapSource(frameBitmap);
+        }
+
+        private void MainCanvas_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Map = new clMap();
+            double x = e.GetPosition(ImageGrid).X;
+            double y = e.GetPosition(ImageGrid).Y;
+
+            double xx = (x - posX) / Map.PixelWidth;
+            double yy = (y - posY) / Map.PixelHeight;
+
+            Map.InvertPixel((int)xx, (int)yy);
+            Drawing();
+        }
+    }
+}
---
+++ WaterfallSwingWPF/clMap.cs
@@ -0,0 +1,300 @@
+﻿  
+using AquaVeilV1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Color = System.Windows.Media.Color;
+
+namespace AquaVeilV1
+{
+    public class clMap
+    {
+        private Int32 _Height = Settings.Height;
+        private Int32 _Width = Settings.Width;
+
+        private Int32 _PixelHeight = Settings.HeightPix;
+        private Int32 _PixelWidth = Settings.WidthPix;
+
+        private Color _ColorPenForeground = Colors.Red;
+        private Color _ColorPenBackground = Colors.White;
+
+        private Color[] _ColumnColor;
+
+        private Int32 _WidthMargin = 2;
+        private Int32 _HeightMargin = 0;
+
+        private Int32[][] _MapCanvas;
+
+        /// <summary>
+        /// Высота поля
+        /// </summary>
+        [DisplayName("Высота")]
+        [Category("Поле")]
+        [Description("Высота поля")]
+        public Int32 Height
+        {
+            set { _Height = value; }
+            get { return _Height; }
+        }
+
+        /// <summary>
+        /// Ширина поля
+        /// </summary>
+        [DisplayName("Ширина")]
+        [Category("Поле")]
+        [Description("Ширина поля")]
+        public Int32 Width
+        {
+            set { _Width = value; }
+            get { return _Width; }
+        }
+
+
+
+        /// <summary>
+        /// Высота пикселя
+        /// </summary>
+        [DisplayName("Высота пикселя")]
+        [Category("Пиксель")]
+        [Description("Ширина поля")]
+        public Int32 PixelHeight
+        {
+            set { _PixelHeight = value; }
+            get { return _PixelHeight; }
+        }
+
+
+        /// <summary>
+        /// Ширина пикселя
+        /// </summary>
+        [DisplayName("Ширина пикселя")]
+        [Category("Пиксель")]
+        [Description("Ширина поля")]
+        public Int32 PixelWidth
+        {
+            set { _PixelWidth = value; }
+            get { return _PixelWidth; }
+        }
+
+
+        /// <summary>
+        /// Ширина пикселя
+        /// </summary>
+        [DisplayName("Цвет пикселя незакрашенного")]
+        [Category("Пиксель")]
+        [Description("Цвет пикселя незакрашенного")]
+        public Color ColorPenBackground
+        {
+            set { _ColorPenBackground = value; }
+            get { return _ColorPenBackground; }
+        }
+
+
+        /// <summary>
+        /// "Цвет пикселя закрашенного
+        /// </summary>
+        [DisplayName("Цвет пикселя закрашенного")]
+        [Category("Пиксель")]
+        [Description("Цвет пикселя закрашенного")]
+        public Color ColorPenForeground
+        {
+            set { _ColorPenForeground = value; }
+            get { return _ColorPenForeground; }
+        }
+
+
+        public Int32[][] MapCanvas
+        {
+            set { _MapCanvas = value; }
+            get { return _MapCanvas; }
+        }
+
+        /// <summary>
+        /// Цвет каждого столбца
+        /// </summary>
+        public Color[] ColumnColor { 
+            get => _ColumnColor; 
+            set => _ColumnColor = value; 
+        }
+
+        /// <summary>
+        /// Растояние между пикселями в длинну
+        /// </summary>
+        public int WidthMargin { 
+            get => _WidthMargin; 
+            set => _WidthMargin = value;
+        }
+
+        /// <summary>
+        /// Растояние между пикселями в ширину
+        /// </summary>
+        public int HeightMargin {
+            get => _HeightMargin; 
+            set => _HeightMargin = value; 
+        }
+
+        public clMap()
+        {
+            _MapCanvas = new Int32[_Width][];
+            for (int i = 0; i < _Width; i++)
+                _MapCanvas[i] = new Int32[_Height];
+
+
+            for (int i = 0; i < _Width; i++)
+                for (int j = 0; j < _Height; j++)
+                    _MapCanvas[i][j] = 0;
+
+            initColor();
+        }
+
+        public void initColor() {
+            ColumnColor = new Color[_Width];
+            for (int i = 0; i < _Width; i++)
+            {
+                ColumnColor[i] = this.ColorPenForeground;
+            }
+        }
+
+        public void CreateCanvas()
+        {
+            _MapCanvas = new Int32[_Width][];
+            for (int i = 0; i < _Width; i++)
+                _MapCanvas[i] = new Int32[_Height];
+
+            for (int i = 0; i < _Width; i++)
+                for (int j = 0; j < _Height; j++)
+                    _MapCanvas[i][j] = 0;
+        }
+
+        public Boolean InvertPixel(Int32 x, Int32 y)
+        {
+            if (x >= Width || x<0)
+                return false;
+
+            if (y >= Height || y<0)
+                return false;
+
+            if (MapCanvas[x][y] == 0)
+                MapCanvas[x][y] = 1;
+            else
+                MapCanvas[x][y] = 0;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Функция для создания картинки по MapCanvas
+        /// </summary>
+        /// <returns>Картинка</returns>
+        public DrawingGroup getBitmapImage()
+        {
+            DrawingGroup drawingGroup = new DrawingGroup();
+
+            GeometryDrawing g = new GeometryDrawing();
+
+            SolidColorBrush bf = new SolidColorBrush(ColorPenBackground);
+
+            for (int i = 0; i < Width; i++)
+                for (int j = 0; j < Height; j++)
+                {
+                    if (MapCanvas[i][j] == 0)
+                        g.Brush = new SolidColorBrush(ColumnColor[i]);
+                    else
+                        g.Brush = bf;
+                    g.Geometry = new RectangleGeometry(new Rect(i * PixelWidth, j * PixelHeight,
+                        PixelWidth - WidthMargin, PixelHeight - HeightMargin));
+                    drawingGroup.Children.Add(g);
+                }
+
+
+            return drawingGroup;
+        }
+
+        public bool changeColumnColor(Color newColor, int colorIndex)
+        {
+            if (colorIndex >= Width || colorIndex < 0)
+                return false;
+            ColumnColor[colorIndex] = newColor;
+            return true;
+        }
+
+        /// <summary>
+        /// Цветовая панель по _ColumnColor
+        /// </summary>
+        /// <returns>Каринка для работы с цветовой панелью</returns>
+        public DrawingGroup getColorsBitmap() {
+            DrawingGroup frameBitmap = new DrawingGroup();
+
+            GeometryDrawing g = new GeometryDrawing();
+
+            SolidColorBrush b;
+
+            for (int i = 0; i < Width; i++)
+            {
+                b = new SolidColorBrush(ColumnColor[i]);
+                g.Brush = b;
+                g.Geometry = new RectangleGeometry(new Rect(i * PixelWidth, 0, PixelWidth - WidthMargin,
+                    PixelHeight - HeightMargin));
+                frameBitmap.Children.Add(g);
+            }
+            return frameBitmap;
+        }
+
+        /// <summary>
+        /// Функция превращения clMap в файлы
+        /// 1 файл - Frame{fileNum}.txt Хранит настройки кадра и сам кадр в виде 
+        /// двумерного массива
+        /// 2 файл - Frame{fileNum}Color.txt Хранит цвета кадра по столбцам
+        /// </summary>
+        /// <param name="fileNum">Часть имени файла</param>
+        /// <param name="Path">Путь где сохранять файл</param>
+        public void printToFiles(int fileNum,string Path) {
+            // 1 файл - Frame{fileNum}.txt
+            string fileText = "";
+            string fileName = @"Frame" + fileNum + ".txt";
+
+            fileText += Convert.ToString(Settings.Width, 16) + "\n";
+            fileText += Convert.ToString(Settings.Height, 16) + "\n";
+
+
+            for (int i = 0; i <MapCanvas[i].Length; i++)
+            {
+                for (int j = 0; j < MapCanvas.Length; j++)
+                {
+                    fileText += MapCanvas[j][i];
+                }
+                fileText += "\n";
+            }
+
+            using (StreamWriter FileFrame = new StreamWriter(Path + @"\" + fileName, false, System.Text.Encoding.Default))
+            {
+                FileFrame.Write(fileText);
+                fileText = "";
+            }
+
+            // 2 файл - Frame{fileNum}Color.txt 
+            fileName = "Frame" + fileNum + "Color.txt";
+
+            for (int i = 0; i < Width; i++)
+            {
+
+                fileText += Convert.ToString(ColumnColor[i].R, 16) + (ColumnColor[i].R == 0 ? "0" : "");
+                fileText += Convert.ToString(ColumnColor[i].G, 16) + (ColumnColor[i].G == 0 ? "0" : "");
+                fileText += Convert.ToString(ColumnColor[i].B, 16) + (ColumnColor[i].B == 0 ? "0" : "");
+                fileText += "\n";
+            }
+
+            using (StreamWriter FileFrame = new StreamWriter(Path + @"\" + fileName, false, System.Text.Encoding.Default))
+            {
+                FileFrame.Write(fileText);
+            }
+        }
+    }
+}
