a05b004a73e3508b4a06e1ea4b4b2f0c2242daa6
RD-1-2020 <mestrx@mail.ru>
2021-06-19 02:03:42 +0700

    Добавил к решению мобильное приложение

---
+++ AquaVeilMobileApp/AquaVeilMobileApp/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaVeilMobileApp
+{
+    static class Settings
+    {
+        static private Int32 _width = 10;
+        static private Int32 _height = 10;
+
+        /// <summary>
+        /// Ширина кадра
+        /// </summary>
+        public static int Width { get => _width; set => _width = value; }
+
+        /// <summary>
+        /// Высота кадра
+        /// </summary>
+        public static int Height { get => _height; set => _height = value; }
+
+        static private Int32 _widthPix = 15;
+        static private Int32 _heightPix = 15;
+
+        /// <summary>
+        /// Ширина пикселя кадра
+        /// </summary>
+        public static int WidthPix { get => _widthPix; set => _widthPix = value; }
+
+        /// <summary>
+        /// Высота пикселя кадра
+        /// </summary>
+        public static int HeightPix { get => _heightPix; set => _heightPix = value; }
+    }
+}
---
+++ AquaVeilMobileApp/AquaVeilMobileApp/SettingsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AquaVeilMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtSubmit_OnClicked(object sender, EventArgs e)
+        {
+            string tmp = "Drink!";
+            eHeight.Text = tmp;
+            eWidth.Text = tmp;
+            Thread.CurrentThread.Abort();
+            await Navigation.PopAsync();
+            await Navigation.PushAsync(new MainPage());
+            
+        }
+    }
+}
